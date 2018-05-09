530aee71d0237d7dff33ee1ad6a61436f9cec016
usmanmohammed <usman.mohammed.17@aberdeen.ac.uk>
2018-05-09 20:06:39 +0100

    Added Db initializer

--- ClickToLoadMore/Models/DatabaseContext.cs
+++ ClickToLoadMore/Models/DatabaseContext.cs
@@ -20,4 +20,26 @@ namespace ClickToLoadMore.Models
             base.OnModelCreating(modelBuilder);
         }
     }
+
+    public static class DbInitializer
+    {
+        public static void Initialize(DatabaseContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Items.Any())
+                return;
+
+            //Populate 20 items
+            var items = new List<Item>();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                items.Add(new Item { ItemName = $"Item {i}", Price = 200 * i });
+            }
+
+            context.Items.AddRange(items);
+            context.SaveChanges();
+        }
+    }
 }

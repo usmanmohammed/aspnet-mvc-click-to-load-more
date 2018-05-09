ed1f1b91359b7038a5f8d52a6a5662597b09da4a
usmanmohammed <usman.mohammed.17@aberdeen.ac.uk>
2018-05-09 18:41:20 +0100

    Added 'Item' model

---
+++ ClickToLoadMore/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickToLoadMore.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+    }
+}

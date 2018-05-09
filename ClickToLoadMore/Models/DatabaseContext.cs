0117ad261577d04046b4f58686df090a25c14b3c
usmanmohammed <usman.mohammed.17@aberdeen.ac.uk>
2018-05-09 18:56:55 +0100

    Added Dabase Context

---
+++ ClickToLoadMore/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickToLoadMore.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=coreDB;Trusted_Connection=True;");
+        }
+    }
+}
